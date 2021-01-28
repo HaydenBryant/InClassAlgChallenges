d03276b4ece9235451d2a741b4b6bc8101db7e4c
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-01-27 22:44:28 -0500

    failed time for kthlargestelm 3/ 16 pass

--- src/com/spring/hockeystats/Main.java
+++ src/com/spring/hockeystats/Main.java
@@ -1,19 +1,19 @@
-boolean containsCloseNums(int[] nums, int k) {
-        Hashtable<Integer, Integer> table = new Hashtable<>();
+int kthLargestElement(int[] nums, int k) {
+        int elm = k - 1;
+        int prev = nums[0];
+        int kthElm = 0;
 
-
-        for(int i = 0; i < nums.length; i++){
-        if(!table.containsKey(nums[i])){
-        table.put(nums[i], i);
+        for(int i = 1; i < nums.length; i++){
+        if(nums[i] == prev){
+        continue;
         } else {
-        if(Math.abs(table.get(nums[i]) - i) <= k){
-        return true;
-        } else {
-        table.put(nums[i], i);
+        prev = nums[i];
+        k--;
+        }
+        if(k == 0){
+        kthElm = prev;
+        break;
         }
         }
-        }
-
-
-        return false;
+        return kthElm;
         }
