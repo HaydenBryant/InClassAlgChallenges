40c3d2a026aa831ee4a9b129561721a5eefb465c
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-02-01 22:31:58 -0500

    re read and finally done 100%

--- src/com/spring/hockeystats/Main.java
+++ src/com/spring/hockeystats/Main.java
@@ -1,15 +1,46 @@
 int kthLargestElement(int[] nums, int k) {
 
-        TreeSet<Integer> tree = new TreeSet<Integer>();
+        // TreeSet<Integer> tree = new TreeSet<Integer>();
 
-        for(int i = 0; i< nums.length; i++) {
-        tree.add(nums[i]);
+        // for(int i = 0; i< nums.length; i++) {
+        //     tree.add(nums[i]);
+        // }
+
+        // Object[] arr = tree.toArray();
+
+        // int kthElm = Integer.parseInt(arr[arr.length - k].toString());
+
+        // return kthElm;
+
+        int kthElm = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int num : nums){
+        if(!map.containsKey(num)){
+        map.put(num, 1);
+        } else {
+        map.put(num, map.get(num) + 1);
+        }
         }
 
-        Object[] arr = tree.toArray();
+        int count = 0;
 
-        int kthElm = Integer.parseInt(arr[arr.length - k].toString());
+        ArrayList<Integer> keys = new ArrayList<Integer>(map.size());
+        keys.addAll(map.keySet());
+        Collections.sort(keys);
+
+        for(int i = keys.size() - 1; i >= 0; i--){
+        count = count + map.get(keys.get(i));
+        if(count >= k){
+        kthElm = keys.get(i);
+        break;
+        }
+        }
 
         return kthElm;
 
         }
+
+
+
