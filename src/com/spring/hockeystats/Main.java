f25b6142314f16e8c56adf4f51071f7b2bccf44e
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-02-02 21:05:07 -0500

    bubble sort. these dont really count. its understanding

--- src/com/spring/hockeystats/Main.java
+++ src/com/spring/hockeystats/Main.java
@@ -1,46 +1,20 @@
-int kthLargestElement(int[] nums, int k) {
+void swap(int[] items, int firstIndex, int secondIndex) {
+        int temp = items[firstIndex];
+        items[firstIndex] = items[secondIndex];
+        items[secondIndex] = temp;
+        }
 
-        // TreeSet<Integer> tree = new TreeSet<Integer>();
+        int[] bubbleSort(int[] items) {
 
-        // for(int i = 0; i< nums.length; i++) {
-        //     tree.add(nums[i]);
-        // }
+        int len = items.length;
 
-        // Object[] arr = tree.toArray();
-
-        // int kthElm = Integer.parseInt(arr[arr.length - k].toString());
-
-        // return kthElm;
-
-        int kthElm = 0;
-
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for(int num : nums){
-        if(!map.containsKey(num)){
-        map.put(num, 1);
-        } else {
-        map.put(num, map.get(num) + 1);
+        for (int i = 0; i < len; i++) {
+        for (int j = 0, stop = len - i; j < stop - 1; j++) {
+        if (items[j] > items[j + 1]) {
+        swap(items, j, j + 1);
+        }
         }
         }
 
-        int count = 0;
-
-        ArrayList<Integer> keys = new ArrayList<Integer>(map.size());
-        keys.addAll(map.keySet());
-        Collections.sort(keys);
-
-        for(int i = keys.size() - 1; i >= 0; i--){
-        count = count + map.get(keys.get(i));
-        if(count >= k){
-        kthElm = keys.get(i);
-        break;
+        return items;
         }
-        }
-
-        return kthElm;
-
-        }
-
-
-
