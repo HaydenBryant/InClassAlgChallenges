3c0ce037f5381753c2bbfc59b99588ac2b51b282
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-02-07 10:22:05 -0500

    easy one this morning. migratory birds

--- src/GroupAlgs/Solution.java
+++ src/GroupAlgs/Solution.java
@@ -4,89 +4,55 @@ import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class Solution {
 
-    // Complete the connectedCell function below.
-    static int connectedCell(int[][] matrix) {
-        int group = 2;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 1) {
-                    dfsUtil(matrix, i, j, group);
-                    group++;
-                }
-            }
-        }
-        //count group size
-        Map<Integer, Integer> counters = new HashMap<>();
-        for (int[] row : matrix) {
-            for (int cell : row) {
-                if (cell == 0) continue;
-                if (!counters.containsKey(cell)) {
-                    counters.put(cell, 1);
-                } else {
-                    counters.put(cell, counters.get(cell) + 1);
-                }
-            }
-        }
-        int result = 0;
-        for (Integer groupCount : counters.keySet()) {
-            result = Math.max(counters.get(groupCount), result);
-        }
-        return result;
-    }
-    public static void dfsUtil(int[][] matrix, int row, int column, int group) {
-        if (row < 0 || column < 0 || row >= matrix.length || column >= matrix[row].length) return;
-        if (matrix[row][column] == 0 || matrix[row][column] > 1) return;
-        matrix[row][column] = group;
-        dfsUtil(matrix, row - 1, column, group);
-        dfsUtil(matrix, row + 1, column, group);
-        dfsUtil(matrix, row, column + 1, group);
-        dfsUtil(matrix, row, column - 1, group);
-        dfsUtil(matrix, row - 1, column + 1, group);
-        dfsUtil(matrix, row + 1, column - 1, group);
-        dfsUtil(matrix, row + 1, column + 1, group);
-        dfsUtil(matrix, row - 1, column - 1, group);
-    }
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<Integer> arr) {
+        HashMap<Integer, Integer> birdmap = new HashMap<>();
 
-    private static final Scanner scanner = new Scanner(System.in);
+        for(int bird : arr){
+            if(!birdmap.containsKey(bird)){
+                birdmap.put(bird, 1);
+            } else {
+                birdmap.put(bird, birdmap.get(bird) + 1);
+            }
+        }
+
+        int maxBird = 1;
+
+        for(int bird : birdmap.keySet()){
+            if(birdmap.get(bird) > birdmap.get(maxBird)){
+                maxBird = bird;
+            } else if (birdmap.get(bird) == birdmap.get(maxBird)){
+                maxBird = Math.min(bird, maxBird);
+            }
+        }
+
+        return maxBird;
+    }
 
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
 
-        int m = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
-        int[][] matrix = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            String[] matrixRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < m; j++) {
-                int matrixItem = Integer.parseInt(matrixRowItems[j]);
-                matrix[i][j] = matrixItem;
-            }
-        }
-
-        int result = connectedCell(matrix);
+        int result = migratoryBirds(arr);
 
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
 
+        bufferedReader.close();
         bufferedWriter.close();
-
-        scanner.close();
     }
 }
-
-
-
-
-
-
