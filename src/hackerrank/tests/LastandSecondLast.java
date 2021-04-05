88a34362af3d045f1f12255eb4a7eec3279b7811
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-04-05 15:17:30 -0400

    last and second last done with loop

---
+++ src/hackerrank/tests/LastandSecondLast.java
@@ -0,0 +1,24 @@
+package hackerrank.tests;
+
+import java.util.ArrayList;
+
+public class LastandSecondLast {
+    public static void main(String[] args) {
+        String string = "apple";
+
+        System.out.println(LastLetter(string));
+    }
+
+    static String LastLetter(String string){
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i = string.length() - 1; i > 0; i--){
+            list.add(Character.toString(string.charAt(i)));
+            if(list.size() == 2){
+                break;
+            }
+        }
+
+        return list.toString();
+    }
+}
