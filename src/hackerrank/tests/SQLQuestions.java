22e2d7929b64f84afbf4eb31b5fc63abafcb9445
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-04-12 21:20:14 -0400

    sql questions

--- src/hackerrank/tests/SQLQuestions.java
+++ src/hackerrank/tests/SQLQuestions.java
@@ -7,5 +7,20 @@ public class SQLQuestions {
                 "WHERE NAME = 'USA'" +
                 "AND WHERE CREDITS > 100000" +
                 "ORDER BY ID ASC";
+
+        String YoungestEmployees =
+                "SELECT UIN, NAME FROM " +
+                        "EMPLOYEE JOIN EMPLOYEE_UIN ON (ID=ID)" +
+                        "WHERE AGE < 25" +
+                        "ORDER BY NAME ASC, ID ASC";
+
+        String OrderManagementSystem =
+                "SELECT ID, NAME FROM CUSTOMER " +
+                        "ORDER BY NAME DESC, ID ASC";
+
+        String TheSuperheroesName =
+                "SELECT NAME FROM SUPERHERO " +
+                        "WHERE LEN(NAME) < 7 " +
+                        "ORDER BY ID ASC";
     }
 }
