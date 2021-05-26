736431eae3126e949c5da0ff401faf36e57d9f09
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-05-26 13:14:23 -0400

    test practice

--- src/hackerrank/tests/CitizenPracticeTest1/MinSwapBracketBalance.java
+++ src/hackerrank/tests/CitizenPracticeTest1/MinSwapBracketBalance.java
@@ -1,52 +1,76 @@
 package hackerrank.tests.CitizenPracticeTest1;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class MinSwapBracketBalance {
     public static void main(String[] args) {
         String brackets = "(()))(";
         String bracketsHard = "))()(()(";
 
-        System.out.println(minimumSwaps(brackets));
+
+        System.out.println(minimumSwaps(bracketsHard));
     }
 
     public static int minimumSwaps(String brackets){
-        int swaps = 0;
+        int minSwaps = 0;
 
-        int open = 0;
-        int close = 0;
-        int current = 0;
+        //create stack
+        //iterate through brackets
+            //put ( in stack
+            //check )
+            //if balanced disregard both
+            //else current enters stack and continue
 
-        String[] arr = brackets.split("");
+        //if stack is empty, return minswaps (balanced so 0 swaps)
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i].equals("(")){
-                open++;
-                current++;
-            }
-            if(arr[i].equals(")")){
-                close++;
-                current--;
-            }
-            if(current < 0){
-                for (int j = i; j < arr.length; j++){
-                    if (arr[j].equals("(")){
-                        arr[i] = "(";
-                        arr[j] = ")";
-                        swaps++;
-                        current = 0;
-                        System.out.println("loop inner run");
-                        break;
-                    }
+        //iterate through stack
+            //count bracket types
 
+        //if brackets are equal
+            //min swaps =   ( / 2 round up?  if odd add 1 divide by 2
+            //return minswaps
+
+        //else
+            //return -1
+
+        Stack<Character> brackStack = new Stack<>();
+
+        for(char bracket : brackets.toCharArray()){
+            if(bracket == '('){
+                brackStack.push(bracket);
+            } else {
+                if(brackStack.empty() || brackStack.peek() == ')'){
+                    brackStack.push(bracket);
+                } else {
+                    brackStack.pop();
                 }
             }
         }
 
-        System.out.println(open);
-        System.out.println(close);
+        if(brackStack.empty()){
+            return minSwaps;
+        }
+
+        int open = 0;
+        int close = 0;
+
+        while (!brackStack.empty()){
+            if(brackStack.pop() == '('){
+                 open++;
+            } else {
+                close++;
+            }
+        }
 
         if(open == close){
-            return swaps;
+            if(open % 2 != 0){
+                open++;
+            }
+            minSwaps = open / 2;
+            return minSwaps;
+        } else {
+            return -1;
         }
-        return -1;
     }
 }
