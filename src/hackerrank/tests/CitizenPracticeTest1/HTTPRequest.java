fb6486620290d1d3f5eb45684563bb87eccbfcbe
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-06-01 20:48:16 -0400

    couldnt get as object without outside package. attempting regex pattern next

--- src/hackerrank/tests/CitizenPracticeTest1/HTTPRequest.java
+++ src/hackerrank/tests/CitizenPracticeTest1/HTTPRequest.java
@@ -1,13 +1,13 @@
 package hackerrank.tests.CitizenPracticeTest1;
 
-import netscape.javascript.JSObject;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HTTPRequest {
     public static void main(String[] args) {
@@ -15,22 +15,24 @@ public class HTTPRequest {
     }
 
     public static List<String> topArticles(String username, int limit){
-        List<String> list = new ArrayList<>();
-
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonmock.hackerrank.com/api/articles?author=" + username + "&page=" + limit)).build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
 
-//                .thenApply(Parse::parsePlayer)
-                .join()
-                .toString();
-
-        return list;
+                .thenApply(HTTPRequest::parse)
+//                .thenAccept(System.out::println)
+                .join();
     }
 
-    public static void parse(JSObject body){
+    public static List<String> parse(String responseBody){
+        List<String> list = new ArrayList<>();
 
+        String regex = "";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+
+        return list;
     }
 }
