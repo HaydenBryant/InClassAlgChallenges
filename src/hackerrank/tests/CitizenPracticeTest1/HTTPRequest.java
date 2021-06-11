ca948262d1bdf6e075cb2c50506f07ff6c8404a8
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-06-11 14:21:56 -0400

    Very close using string builder

--- src/hackerrank/tests/CitizenPracticeTest1/HTTPRequest.java
+++ src/hackerrank/tests/CitizenPracticeTest1/HTTPRequest.java
@@ -1,38 +1,38 @@
-package hackerrank.tests.CitizenPracticeTest1;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class HTTPRequest {
-    public static void main(String[] args) {
-        topArticles("olalonde", 1);
-    }
-
-    public static List<String> topArticles(String username, int limit){
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonmock.hackerrank.com/api/articles?author=" + username + "&page=" + limit)).build();
-        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-
-                .thenApply(HTTPRequest::parse)
-//                .thenAccept(System.out::println)
-                .join();
-    }
-
-    public static List<String> parse(String responseBody){
-        List<String> list = new ArrayList<>();
-
-        String regex = "";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(s);
-
-        return list;
-    }
-}
+//package hackerrank.tests.CitizenPracticeTest1;
+//
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//public class HTTPRequest {
+//    public static void main(String[] args) {
+//        topArticles("olalonde", 1);
+//    }
+//
+//    public static List<String> topArticles(String username, int limit){
+//
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonmock.hackerrank.com/api/articles?author=" + username + "&page=" + limit)).build();
+//        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+//                .thenApply(HttpResponse::body)
+//
+//                .thenApply(HTTPRequest::parse)
+////                .thenAccept(System.out::println)
+//                .join();
+//    }
+//
+//    public static List<String> parse(String responseBody){
+//        List<String> list = new ArrayList<>();
+//
+//        String regex = "";
+//        Pattern pattern = Pattern.compile(regex);
+//        Matcher matcher = pattern.matcher(s);
+//
+//        return list;
+//    }
+//}
--- src/hackerrank/tests/CitizenPracticeTest2/SlotMachine.java
+++ src/hackerrank/tests/CitizenPracticeTest2/SlotMachine.java
@@ -1,26 +1,48 @@
 package hackerrank.tests.CitizenPracticeTest2;
 
+import TestDome.InternalNodes;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SlotMachine {
 
     public static void main(String[] args) {
+        ArrayList<String> history = new ArrayList<>();
 
+        history.add("137");
+        history.add("364");
+        history.add("115");
+        history.add("724");
+
+        System.out.println(slotWheels(history));
     }
 
-    public int slotWheels(List<String> history){
+    public static int slotWheels(List<String> history){
         int totalSpins = 0;
         int wheels = history.get(0).length();
 
+        ArrayList<Integer> maxList = new ArrayList<>();
+
         for(int i = wheels; i > 0; i--){
-            ArrayList<Integer> maxList = new ArrayList<>();
-            for(String s : history){
 
-                for(String num : s.split("")){
-
+            for(int j = 0; j < history.size(); j++){
+                StringBuilder s = new StringBuilder(history.get(j));
+                int max = 0;
+                int maxDex = 0;
+                for(int k = 0; k < s.length(); k++){
+                    int temp = Character.getNumericValue(s.charAt(k));
+                    if(temp > max){
+                        max = temp;
+                        maxDex = k;
+                    }
                 }
+                maxList.add(max);
+                s.setCharAt(maxDex, '0');
             }
+            totalSpins+= Collections.max(maxList);
+            maxList = new ArrayList<>();
         }
 
         return totalSpins;
