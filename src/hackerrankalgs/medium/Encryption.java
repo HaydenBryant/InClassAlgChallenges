a4c3cc52c91e8751473f913493c6f1b0c8b06560
Hayden <haydenbryant.student@careerdevs.com>
2021-09-05 02:22:56 -0400

    trying to work through using the loops and nested lists. After completing i ill try to solve with more math and less loops

--- src/hackerrankalgs/medium/Encryption.java
+++ src/hackerrankalgs/medium/Encryption.java
@@ -1,5 +1,8 @@
 package hackerrankalgs.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Encryption {
     public static void main(String[] args) {
         String s = "Have a nice day";
@@ -10,15 +13,35 @@ public class Encryption {
     public static String encryption(String s) {
         // Write your code here
         String noSpace = s.replaceAll("\\s", "");
+        int noSpaceLen = noSpace.length();
+        List<List<String>> nonEncrypt = new ArrayList<List<String>>();
 
-        double high = Math.ceil(Math.sqrt(noSpace.length()));
-        double low = Math.floor(Math.sqrt(noSpace.length()));
+        double high = Math.ceil(Math.sqrt(noSpaceLen));
+        double low = Math.floor(Math.sqrt(noSpaceLen));
 
-        //make 2d list
-        //for loop creating single list of size high executing low times
-        //add lists to 2d list
+        List<String> temp = new ArrayList<>();
+        for(int i = 0; i < noSpaceLen; i++){
+            if(temp.size() < high){
+                temp.add(Character.toString(noSpace.charAt(i)));
+            } else {
+                System.out.println("here");
+                nonEncrypt.add(temp);
+                temp = new ArrayList<>();
+                temp.add(Character.toString(noSpace.charAt(i)));
+            }
+            if(i + 1 >= noSpaceLen){
+                System.out.println("here");
+                nonEncrypt.add(temp);
+            }
+        }
 
-        //nested for loop over 2d list to create encryption
+        String encrypted = "";
+
+        for(int i = 0; i < nonEncrypt.size(); i++){
+            for(int j = 0; j < nonEncrypt.get(i).size(); j++){
+
+            }
+        }
 
         return noSpace;
 
