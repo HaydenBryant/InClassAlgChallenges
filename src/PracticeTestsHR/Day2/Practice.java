7dd5639f49d5e1e6d1a11a5d670695f3089dc5bd
Hayden <haydenbryant.student@careerdevs.com>
2022-04-11 16:36:34 -0400

    diagonal difference

--- src/PracticeTestsHR/Day2/Practice.java
+++ src/PracticeTestsHR/Day2/Practice.java
@@ -11,7 +11,15 @@ public class Practice {
         List<Integer> a = new ArrayList<>(Arrays.asList(0, 0, 1, 2, 1));
         System.out.println(lonelyinteger(a));
 
-
+        //Diagonal Difference
+        List<List<Integer>> arr = new ArrayList<>();
+        List<Integer> one = new ArrayList<>(Arrays.asList(11, 2, 4));
+        List<Integer> two = new ArrayList<>(Arrays.asList(4, 5, 6));
+        List<Integer> three = new ArrayList<>(Arrays.asList(10, 8, -12));
+        arr.add(one);
+        arr.add(two);
+        arr.add(three);
+        System.out.println(diagonalDifference(arr));
     }
 
     public static int lonelyinteger(List<Integer> a) {
@@ -29,6 +37,19 @@ public class Practice {
             }
         }
         return ans;
+    }
 
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int left = 0;
+        int right = 0;
+        int arrSize = arr.size() - 1;
+
+        for(int i = 0; i < arr.size(); i++){
+            left += arr.get(i).get(i);
+            right += arr.get(i).get(arrSize - i);
+        }
+
+        return Math.abs(left - right);
     }
 }
