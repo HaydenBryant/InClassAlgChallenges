70f645669e63a42e617f1687dda73fea93758b40
Hayden <haydenbryant.student@careerdevs.com>
2022-04-11 16:49:26 -0400

    lonely int

--- src/PracticeTestsHR/Day2/Practice.java
+++ src/PracticeTestsHR/Day2/Practice.java
@@ -1,9 +1,6 @@
 package PracticeTestsHR.Day2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
@@ -20,6 +17,16 @@ public class Practice {
         arr.add(two);
         arr.add(three);
         System.out.println(diagonalDifference(arr));
+
+        //counting sort
+        List<Integer> arrcs = new ArrayList<>(Arrays.asList(
+                63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99,
+                12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30,
+                27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23,
+                41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33,
+                60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33
+        ));
+        System.out.println(countingSort(arrcs));
     }
 
     public static int lonelyinteger(List<Integer> a) {
@@ -51,5 +58,16 @@ public class Practice {
         }
 
         return Math.abs(left - right);
+    }
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        List<Integer> ans = new ArrayList<Integer>(Collections.nCopies(100, 0));
+
+        for(Integer num : arr){
+            ans.set(num, ans.get(num)+1);
+        }
+
+        return ans;
     }
 }
