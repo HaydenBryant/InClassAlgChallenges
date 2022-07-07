b404279cf6d9e95d5c6c8b8c346d0336bbbe9f67
Hayden <haydenbryant.student@careerdevs.com>
2022-07-07 13:55:58 -0400

    more complex with checking for letters in position. need to see if it can be refactored or cleaned up for production. Functions correctly tho

--- src/personal/StringParser.java
+++ src/personal/StringParser.java
@@ -8,9 +8,11 @@ import java.util.List;
 public class StringParser {
     public static void main(String[] args) {
         List<String> dbList = new ArrayList<>(Arrays.asList("stamp", "bingo", "drain", "fluff"));
-        List<Character> presentLetters = new ArrayList<>(Arrays.asList('i'));
-        List<Character> nonPresentLetters = new ArrayList<>(Arrays.asList());
-        System.out.println(parser(dbList, presentLetters, nonPresentLetters));
+        List<Character> presentLetters = new ArrayList<>(Arrays.asList('i', 'n'));
+        List<Character> nonPresentLetters = new ArrayList<>(Arrays.asList('f'));
+        HashMap<Integer, Character> letterLocation = new HashMap<>();
+        letterLocation.put(0, 'd');
+        System.out.println(parserTwo(dbList, presentLetters, letterLocation, nonPresentLetters));
     }
 
     public static List<String> parser(List<String> dbList,
@@ -46,6 +48,40 @@ public class StringParser {
                                       List<Character> presentLetters,
                                       HashMap<Integer, Character> letterLocation,
                                       List<Character> nonPresentLetters){
-        return dbList;
+
+        List<String> possibleWords = new ArrayList<>();
+        String nonPresentLetRegex = ".*[";
+        for(char let : nonPresentLetters){
+            nonPresentLetRegex += let;
+        }
+        nonPresentLetRegex += " ].*";
+
+        String presentLetRegex = ".*[";
+        for(char let : presentLetters){
+            presentLetRegex += let;
+        }
+        presentLetRegex += " ].*";
+
+        for(String word : dbList){
+            //could be string utils from apache but using regex
+            if(word.matches(nonPresentLetRegex)){
+                continue;
+            }
+            if(!word.matches(presentLetRegex)){
+                continue;
+            }
+            boolean correctFlag = true;
+            for(int num : letterLocation.keySet()){
+                if(word.charAt(num) != letterLocation.get(num)){
+                    correctFlag = false;
+                    break;
+                }
+            }
+            if(correctFlag){
+                possibleWords.add(word);
+            }
+        }
+
+        return possibleWords;
     }
 }
